540530415d5132b346492fbb36e2662bc2549f6c
baka4n <qq474899581@gmail.com>
2023-06-15 02:44:48 +0800

    更新设定和各种类

---
+++ src/main/java/union/xenfork/misty/records/MistyRecords.java
@@ -0,0 +1,10 @@
+package union.xenfork.misty.records;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MistyRecords implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
