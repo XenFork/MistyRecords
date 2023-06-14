59a14d573b93c6b579cd510b6b14bb91f89e9a7f
baka4n <qq474899581@gmail.com>
2023-06-15 03:49:57 +0800

    update

--- src/main/java/union/xenfork/misty/records/MistyRecords.java
+++ src/main/java/union/xenfork/misty/records/MistyRecords.java
@@ -3,6 +3,7 @@ package union.xenfork.misty.records;
 import net.fabricmc.api.ModInitializer;
 
 public class MistyRecords implements ModInitializer {
+    public static final String modid = "misty_records";
     @Override
     public void onInitialize() {
 
