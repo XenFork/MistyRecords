07eb84920894accb141390a824a13615d87d769f
baka4n <qq474899581@gmail.com>
2023-06-15 09:26:04 +0800

    Update CultivationOfImmortalsScreen.java

--- src/main/java/union/xenfork/misty/records/screen/handle/CultivationOfImmortalsScreen.java
+++ src/main/java/union/xenfork/misty/records/screen/handle/CultivationOfImmortalsScreen.java
@@ -1,13 +1,16 @@
 package union.xenfork.misty.records.screen.handle;
 
 import io.wispforest.owo.ui.base.BaseOwoScreen;
+import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.component.Components;
+import io.wispforest.owo.ui.component.TextAreaComponent;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.core.*;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
@@ -28,9 +31,19 @@ public class CultivationOfImmortalsScreen extends BaseOwoScreen<FlowLayout> {
                 .surface(Surface.VANILLA_TRANSLUCENT)
                 .horizontalAlignment(HorizontalAlignment.CENTER)
                 .verticalAlignment(VerticalAlignment.CENTER);
+        TextAreaComponent textArea = Components.textArea(Sizing.fixed(50), Sizing.fixed(30), "");
+        ButtonComponent button = Components.button(Text.literal("enter"), buttonComponent -> {
+            if (textArea.getText().equals("123")) {
+                System.out.println("success");
+            } else {
+                System.out.println("error");
+            }
+            textArea.setText("");
+        });
         rootComponent.child(
                 Containers.verticalFlow(Sizing.content(), Sizing.content())
-                        .child(Components.textArea(Sizing.fixed(50), Sizing.fixed(30), "123"))
+                        .child(textArea)
+                        .child(button)
                         .padding(Insets.of(100))
                         .surface(Surface.DARK_PANEL)
                         .verticalAlignment(VerticalAlignment.CENTER)
